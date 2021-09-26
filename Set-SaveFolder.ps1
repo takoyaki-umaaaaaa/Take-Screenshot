a7e5c7f032153cb7667f893c64c6d3add973a0fc
Takoyaki_umaaaaaa <takoyaki_umaaaaaa@hotmail.com>
2021-09-27 01:22:23 +0900

    画像保存先フォルダ選択ダイアログを追加

--- Set-SaveFolder.ps1
+++ Set-SaveFolder.ps1
@@ -1,5 +1,23 @@
 Add-Type -AssemblyName system.windows.forms
 
+
+
+
+# 少しだけ今どきの Control表示にする
+[System.Windows.Forms.Application]::EnableVisualStyles()
+[System.Windows.Forms.VisualStyles]::VisualStyleState = 0
+
+# Win32apiをimport
+Add-Type -MemberDefinition @"
+[DllImport("user32.dll", SetLastError=true)]
+public static extern short SetThreadDpiAwarenessContext(short dpiContext);
+"@ -Namespace Win32 -Name NativeMethods
+
+# 高DPI対応済み設定に変更(フォルダ選択ダイアログをぼやけた表示にさせないため)
+[int]$DpiOldSetting = [Win32.NativeMethods]::SetThreadDpiAwarenessContext(-4)
+
+[System.Windows.Forms.MessageBox]::Show("This is my msgbox")
+
 $fbDlg = New-Object System.Windows.Forms.FolderBrowserDialog
 $fbDlg.Description = "画像を保存するフォルダを選択してください。"
 $fbDlg.SelectedPath = $PSScriptRoot		# Default folderはひとまず Scriptがある場所にする

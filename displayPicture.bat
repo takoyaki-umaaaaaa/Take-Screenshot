647454ef149a86d6a17a71061472d40d79c3e377
Takoyaki_umaaaaaa <takoyaki_umaaaaaa@hotmail.com>
2021-10-14 22:44:37 +0900

    Delete displayPicture.bat

--- displayPicture.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd %~dp0
-
-rem powershell -ExecutionPolicy RemoteSigned -WindowStyle Hidden -File ".\DisplayPicture.ps1"
-powershell -ExecutionPolicy RemoteSigned -File ".\DisplayPicture.ps1"
-
-popd
-pause
